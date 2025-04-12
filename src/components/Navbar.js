b3d8aa44829197ab43cdca24f41be16dd5d4052a
haider <haideraltaf804@gmail.com>
2025-04-12 01:10:11 -0400

    s

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,12 +5,32 @@ import {
   Toolbar, 
   Typography, 
   Button, 
-  Box
+  Box,
+  Avatar,
+  Menu,
+  MenuItem,
+  IconButton,
+  Divider
 } from '@mui/material';
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import AccountCircle from '@mui/icons-material/AccountCircle';
 
-const Navbar = ({ user }) => {
+const Navbar = ({ user, onLogout }) => {
   const location = useLocation();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    handleClose();
+    onLogout();
+  };
 
   return (
     <AppBar position="static" elevation={0} sx={{ 
@@ -36,16 +56,49 @@ const Navbar = ({ user }) => {
           </Typography>
         </Box>
 
-        <Box>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
           {user ? (
-            <Button 
-              component={Link} 
-              to="/workout-plans"
-              variant={location.pathname === '/workout-plans' ? 'contained' : 'text'}
-              sx={{ ml: 2 }}
-            >
-              My Plans
-            </Button>
+            <>
+              <Button 
+                component={Link} 
+                to="/workout-plans"
+                variant={location.pathname === '/workout-plans' ? 'contained' : 'text'}
+                sx={{ ml: 2 }}
+              >
+                My Plans
+              </Button>
+              
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+                sx={{ ml: 2 }}
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem disabled>{user.username}</MenuItem>
+                <Divider />
+                <MenuItem onClick={handleLogout}>Logout</MenuItem>
+              </Menu>
+            </>
           ) : (
             <>
               <Button 
